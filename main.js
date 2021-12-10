f3de113dde17dae408fa2b2ec96fa6bc3bf4b3f0
nastasijaperovic <nastasija.perovic.13.20@ict.edu.rs>
2021-12-10 17:55:57 +0100

    updates on rockets

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ function populateNavigation () {
 
 }
 populateNavigation();
-
+//dynamic navigation
 
 //mobile navigation
 
@@ -60,3 +60,62 @@ menu.addEventListener('click',function(){
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+//mobile navigation
+
+//slider animation JQuery
+$(function(){
+  //config
+  var width=720;
+  var animationSpeed=1000;
+  var pause=3000;
+//cache DOM
+var $slider=$('#slider');
+var $slideContainer=$slider.find('.slides');
+var $slides=$slideContainer.find('.slide');
+var currentSlide=1;
+//defining interval here because of var function scope 
+var interval;
+function startSlider()
+{
+     interval= setInterval(function()
+    {
+        $slideContainer.animate(
+          {'margin-left': '-='+width},
+          animationSpeed,
+          function(){
+            //checking if we are on the first slide
+            currentSlide++;
+            if(currentSlide==$slides.length){
+              currentSlide=1;
+              $slideContainer.css('margin-left',0);
+            }
+          });
+    },pause);
+}
+
+function stopSlider(){
+  clearInterval(interval);
+}
+
+  //setInterval
+  //animate margin-left
+  //if it's last slide,go to pos 1 0px
+$slider.on('mouseenter',stopSlider).on('mouseleave',startSlider); 
+  //listen to mousecenter and posuse
+  //resume on mouseleave
+startSlider();
+});
+
+//slider animation JQuery
+
+//button function
+document.getElementById("aurora8").onclick = function () {
+  location.href = "aurora8.html";
+};
+document.getElementById("capella").onclick = function () {
+  location.href = "capella.html";
+};
+document.getElementById("seriousUltra").onclick = function () {
+  location.href = "siriousultra.html";
+};
